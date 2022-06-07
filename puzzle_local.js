b07a1883c3b4f4440319c767ad1e25a06c73efa3
Jan Corazza <corazzajan@gmail.com>
2022-06-08 00:03:43 +0200

    done

---
+++ puzzle_local.js
@@ -0,0 +1,34 @@
+let puzzle = [];
+
+puzzle[0] =
+{
+    name: "Easy",
+    size: 5,
+    color: [ "blue", "yellow", "green", "red" ],
+    A_row: [ 1, 4, 1, 2 ],
+    A_col: [ 2, 1, 4, 4 ],
+    B_row: [ 1, 5, 2, 3 ],
+    B_col: [ 5, 5, 2, 3 ]
+};
+
+puzzle[1] =
+{
+    name: "Medium",
+    size: 7,
+    color: [ "yellow", "green", "blue", "red", "pink" ],
+    A_row: [ 6, 2, 2, 1, 2 ],
+    A_col: [ 3, 5, 2, 4, 6 ],
+    B_row: [ 3, 7, 3, 7, 4 ],
+    B_col: [ 5, 1, 4, 5, 4 ]
+};
+
+puzzle[2] =
+{
+    name: "Hard",
+    size: 9,
+    color: [ "red", "green", "blue", "yellow", "pink", "orange", "purple", "black", "plum" ],
+    A_row: [ 2, 8, 2, 1, 9, 1, 1, 3, 1 ],
+    A_col: [ 8, 2, 3, 5, 6, 9, 6, 2, 1 ],
+    B_row: [ 8, 7, 4, 9, 4, 8, 6, 5, 6 ],
+    B_col: [ 6, 4, 2, 4, 7, 8, 6, 2, 2 ]
+};
